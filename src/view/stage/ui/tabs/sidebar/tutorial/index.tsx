7c58b3c989b45c37c3a7f2e9cd932c82a50db150
Ibiyemi Abiodun <iaa34@cornell.edu>
2020-08-01 17:08:37 -0400

    Give docs a separate Redux store

--- src/view/stage/ui/tabs/sidebar/tutorial/index.tsx
+++ src/view/stage/ui/tabs/sidebar/tutorial/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
 import { Docs } from './docs';
 
 import '@resources/style/react/ui/tutorial.scss';
 
+import { gameReducer } from '@/store/reducer/game';
+import { undoableReducer } from '@/store/reducer/undo';
+
+// docs get their own Redux store so that their state changes do not interfere
+// with game
+const rootReducer = combineReducers({
+  game: undoableReducer(gameReducer),
+});
+
+const tutorialStore = createStore(rootReducer);
+
 export const TutorialTab: React.FC = () => {
   return (
     <div id='reduct-tutorial'>
-      <Docs />
+      <Provider store={tutorialStore}>
+        <Docs />
+      </Provider>
     </div>
   );
 };
