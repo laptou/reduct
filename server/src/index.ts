f1e1cc6989b2c9381a58d2c378fbf897817d00c0
Ibiyemi Abiodun <iaa34@cornell.edu>
2020-08-12 20:11:39 -0400

    Cornell auth works

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import Koa from 'koa';
+import Koa, { Context, DefaultState } from 'koa';
 import KoaRouter from 'koa-router';
 import KoaSession from 'koa-session';
 import KoaPassport from 'koa-passport';
@@ -12,6 +12,9 @@ import {
 const server = new Koa();
 server.keys = ['secret key, change me before deploying'];
 
+// derived from https://it.cornell.edu/shibboleth/shibboleth-faq
+const NETID_URN = 'urn:oid:0.9.2342.19200300.100.1.1';
+
 KoaPassport.use(new SamlStrategy({
   path: '/login/callback',
   entryPoint: 'https://shibidp-test.cit.cornell.edu/idp/profile/SAML2/Redirect/SSO',
@@ -19,29 +22,47 @@ KoaPassport.use(new SamlStrategy({
   name: 'saml',
 }, (profile: SamlProfile, callback: SamlVerifiedCallback) => {
   console.log('verified');
+  const netId = profile[NETID_URN];
+  callback(null, { netId });
 }));
 
+KoaPassport.serializeUser(({ netId }: {netId: string}, callback) => {
+  callback(null, netId);
+});
+
+KoaPassport.deserializeUser((netId: string, callback) => {
+  callback(null, { netId });
+});
+
 server.use(KoaBodyParser());
-server.use(KoaSession(server));
+server.use(KoaSession({}, server));
 server.use(KoaPassport.initialize());
 server.use(KoaPassport.session());
 
-const router = new KoaRouter();
+const router = new KoaRouter<DefaultState, Context>();
 
-router.get('/',
-  KoaPassport.authenticate('saml'),
-  ctx => {
-    ctx.response.body = 'welcome to reduct';
-  }
+router.get('/login', KoaPassport.authenticate('saml'));
+
+router.post(
+  '/login/callback',
+  KoaPassport.authenticate('saml', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  }),
 );
 
-router.post('/login/callback',
-  KoaPassport.authenticate('saml', {
-    failureRedirect: '/',
-    failureFlash: true,
-  }),
+router.use((ctx, next) => {
+  if (ctx.isAuthenticated()) {
+    return next();
+  } else {
+    ctx.redirect('/login');
+  }
+});
+
+router.get(
+  '/',
   ctx => {
-    ctx.redirect('/');
+    ctx.response.body = `welcome ${JSON.stringify(ctx.state.user)}`;
   }
 );
 
