28c7b1692299e112ffbc0f52767f014d8a822583
Ibiyemi Abiodun <iaa34@cornell.edu>
2020-08-16 18:36:07 -0400

    Add flair to feedback collector

--- client/src/view/stage/ui/feedback-collector.tsx
+++ client/src/view/stage/ui/feedback-collector.tsx
@@ -1,5 +1,7 @@
 import '@resources/style/react/ui/feedback.scss';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, {
+  useCallback, useEffect, useState, useRef,
+} from 'react';
 import { animated, config, useTransition } from 'react-spring';
 
 import { log } from '@/logging/logger';
@@ -9,32 +11,30 @@ const FEEDBACK_COLLECTOR_INTERVAL = 1 * 1000;
 
 export const FeedbackCollectorPopup: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const sendFeedback = useCallback((affect: string) => {
     log('user:feedback', { affect });
     setIsVisible(false);
+    setIsExpanded(false);
   }, [setIsVisible]);
 
   useEffect(() => {
     if (!isVisible) {
-      setTimer(
-        setTimeout(
-          () => {
-            setIsVisible(true);
-          },
-          FEEDBACK_COLLECTOR_INTERVAL
-        )
+      const timerHandle = setTimeout(
+        () => {
+          setIsVisible(true);
+        },
+        FEEDBACK_COLLECTOR_INTERVAL
       );
+
+      return () => clearInterval(timerHandle);
     }
   }, [isVisible]);
 
-  useEffect(() => {
-    if (timer !== null) {
-      return () => clearInterval(timer);
-    }
-  }, [timer]);
+  const buttonContainerDiv = useRef<HTMLDivElement>(null);
 
-  const transition = useTransition(isVisible, null, {
+  const dialogTransition = useTransition(isVisible, null, {
     from: {
       transform: 'translateX(100%) translateX(1rem)',
       opacity: 0,
@@ -45,6 +45,38 @@ export const FeedbackCollectorPopup: React.FC = () => {
     },
     leave: {
       transform: 'translateX(100%) translateX(1rem)',
+      opacity: 0,
+    },
+    config: config.gentle,
+  });
+
+  const hintTransition = useTransition(!isExpanded, null, {
+    from: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  const buttonsTransition = useTransition(isExpanded, null, {
+    from: {
+      height: 0,
+      opacity: 0,
+    },
+    enter(isExpanded) {
+      return (async (next: (props: any) => Promise<void>) => {
+        // wait for div to actually be created
+        await next({});
+
+        // measure element and do the actual animation
+        const height = buttonContainerDiv.current?.scrollHeight ?? 0;
+        await next({
+          height: isExpanded ? height : 0,
+          opacity: 1,
+        });
+
+        // cast to any b/c typedefs don't support this
+      }) as any;
+    },
+    leave: {
+      height: 0,
       opacity: 0,
     },
     config: config.gentle,
@@ -61,29 +93,53 @@ export const FeedbackCollectorPopup: React.FC = () => {
 
   return (
     <div id='reduct-feedback-collector'>
-      {
-        transition.map(({ item, key, props }) => item && (
-          <animated.div style={props} key={key} id='reduct-feedback-collector-popup'>
-            <p id="reduct-feedback-collector-prompt">
-              Hey, can you tell us how you&apos;re feeling?
-            </p>
-            <div id="reduct-feedback-collector-btns">
-              {
-                feedbackTypes.map(([codeName, displayName]) => (
-                  <button
-                    type='button'
-                    key={codeName}
-                    className='btn btn-secondary reduct-feedback-collector-btn'
-                    onClick={() => sendFeedback(codeName)}
-                  >
-                    {displayName}
-                  </button>
-                ))
-              }
-            </div>
-          </animated.div>
-        ))
-      }
+
+      {dialogTransition.map(({ item, key, props }) => item && (
+        <animated.div
+          id='reduct-feedback-collector-popup'
+          style={props} key={key}
+          onMouseEnter={() => setIsExpanded(true)}
+          onMouseLeave={() => setIsExpanded(false)}
+        >
+          <p id="reduct-feedback-collector-prompt">
+            Hey, can you tell us how you&apos;re feeling?
+          </p>
+
+          {hintTransition.map(({ item, key, props }) => item && (
+            <animated.span
+              id="reduct-feedback-collector-hint"
+              style={props} key={key}
+            >
+              Mouse over this popup
+            </animated.span>
+          ))}
+
+          {buttonsTransition.map(({ item, key, props }) => item && (
+            <animated.div
+              id="reduct-feedback-collector-content"
+              style={props} key={key}
+              ref={buttonContainerDiv}
+            >
+              <div id="reduct-feedback-collector-btns" >
+                {
+                  feedbackTypes.map(([codeName, displayName]) => (
+                    <button
+                      type='button'
+                      key={codeName}
+                      className='btn btn-secondary reduct-feedback-collector-btn'
+                      onClick={() => sendFeedback(codeName)}
+                    >
+                      {displayName}
+                    </button>
+                  ))
+                }
+              </div>
+            </animated.div>
+          ))}
+
+        </animated.div>
+      ))}
+
     </div>
   );
 };
