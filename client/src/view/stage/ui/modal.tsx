c0eca093b7f5b3122cbd4da14f7eaa5f0da10856
Ibiyemi Abiodun <iaa34@cornell.edu>
2020-08-17 15:11:15 -0400

    Modal overlay is positioned correctly

--- client/src/view/stage/ui/modal.tsx
+++ client/src/view/stage/ui/modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
+import '@resources/style/react/ui/modals.scss';
 
 export const Modal: React.FC = (props) => {
   return createPortal(
