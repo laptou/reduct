46a612d99dfc0b47d9f0949f8276389c88e3d782
Ibiyemi Abiodun <iaa34@cornell.edu>
2020-08-17 15:32:43 -0400

    Add animation to victory modal

--- client/src/view/stage/ui/modal.tsx
+++ client/src/view/stage/ui/modal.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { createPortal } from 'react-dom';
 import '@resources/style/react/ui/modals.scss';
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { animated, useSpring } from 'react-spring';
 
 export const Modal: React.FC<React.HTMLAttributes<HTMLDivElement>> =
   (props) => {
+    const [animatedStyleProps, setAnimatedStyleProps] =
+      useSpring(() => ({
+        opacity: 0,
+      }));
+
+    useEffect(() => {
+      setAnimatedStyleProps({ opacity: 1 });
+      return () => setAnimatedStyleProps({ opacity: 0 });
+    });
+
     return createPortal(
-      <div id="reduct-modal-overlay" {...props} />,
+      <animated.div
+        id="reduct-modal-overlay"
+        style={animatedStyleProps}
+        {...props}
+      />,
       document.getElementById('reduct-modal')!
     );
   };
--- client/src/view/stage/ui/modals/victory.tsx
+++ client/src/view/stage/ui/modals/victory.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { animated, config as springConfig, useSpring } from 'react-spring';
 
 import { Modal } from '../modal';
 
-import LevelCompleteText from '@resources/graphics/titles/level-complete.svg';
-import { DeepReadonly } from '@/util/helper';
-import { GlobalState, GameMode } from '@/store/state';
 import { createStartLevel } from '@/store/action/game';
+import { GlobalState } from '@/store/state';
+import { DeepReadonly } from '@/util/helper';
+import LevelCompleteText from '@resources/graphics/titles/level-complete.svg';
 
 interface VictoryStoreProps {
-  isVictory: boolean;
   nextLevel: number;
 }
 
@@ -18,32 +18,41 @@ interface VictoryDispatchProps {
 }
 
 const VictoryImpl = (props: VictoryStoreProps & VictoryDispatchProps) => {
-  if (!props.isVictory)
-    return null;
+  const [animatedStyleProps, setAnimatedStyleProps] =
+    useSpring(() => ({
+      transform: 'scale(0)',
+      config: springConfig.stiff,
+    }));
+
+  useEffect(() => void setAnimatedStyleProps({ transform: 'scale(1)' }));
 
   return (
-    <Modal className='reduct-level-modal'>
-      <img
-        src={LevelCompleteText}
-        className='reduct-level-modal-title'
-      />
+    <Modal>
+      <animated.div
+        className='reduct-level-modal'
+        style={animatedStyleProps}
+      >
+        <img
+          src={LevelCompleteText}
+          className='reduct-level-modal-title'
+        />
 
-      <div className='reduct-level-modal-actions'>
-        <button
-          type='button'
-          onClick={() => props.startLevel(props.nextLevel)}
-          className='btn btn-primary'
-        >
-          Next level
-        </button>
-      </div>
+        <div className='reduct-level-modal-actions'>
+          <button
+            type='button'
+            onClick={() => props.startLevel(props.nextLevel)}
+            className='btn btn-primary'
+          >
+            Next level
+          </button>
+        </div>
+      </animated.div >
     </Modal>
   );
 };
 
 export const VictoryOverlay = connect(
   (store: DeepReadonly<GlobalState>) => ({
-    isVictory: store.game.$present.mode === GameMode.Victory,
     nextLevel: store.game.$present.level + 1,
   }),
   (dispatch) => ({
