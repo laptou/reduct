a1d01e8600ceda73388b76002d192e2b6a182f2d
Ibiyemi Abiodun <iaa34@cornell.edu>
2020-08-22 00:42:05 -0400

    Encapsulate timer into separate component

--- client/src/view/stage/ui/tabs/menu/preferences.tsx
+++ client/src/view/stage/ui/tabs/menu/preferences.tsx
@@ -1,19 +1,15 @@
-import React, {
-  useCallback, useState, useEffect, useRef, useMemo,
-} from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import { createEnableSound } from '@/store/action/preferences';
-import { GlobalState } from '@/store/state';
+import { GlobalState, ResearchConsentState } from '@/store/state';
 import { DeepReadonly } from '@/util/helper';
 import '@resources/style/react/ui/preferences.scss';
 import { createToggleCredits } from '@/store/action/game';
-import { GAME_TIME_LIMIT } from '@/util/constants';
 
 interface PreferencesStoreProps {
   isSoundEnabled: boolean;
-  isResearchEnabled: boolean;
-  endTime: number;
+  isResearchEnabled: ResearchConsentState;
 }
 
 interface PreferencesDispatchProps {
@@ -27,61 +23,12 @@ const PreferencesImpl: React.FC<PreferencesProps> = (props) => {
   const {
     isSoundEnabled,
     isResearchEnabled,
-    endTime,
     enableSound,
     toggleCredits,
   } = props;
 
-  const [currentTime, setCurrentTime] = useState(+new Date());
-
-  const timerRef = useRef<number>();
-
-  const timerCallback = useCallback(() => {
-    setCurrentTime(+new Date());
-  }, []);
-
-  useEffect(() => {
-    if (currentTime < endTime) {
-      timerRef.current = setTimeout(timerCallback, 1000) as unknown as number;
-
-      return () => {
-        clearTimeout(timerRef.current);
-        timerRef.current = undefined;
-      };
-    }
-  }, [currentTime, endTime, timerCallback]);
-
-  const remainingTime = useMemo(
-    () => {
-      const totalMillis = endTime - currentTime;
-      const totalSeconds = Math.floor(totalMillis / 1000);
-      const totalMinutes = Math.floor(totalMillis / 60 / 1000);
-      const totalHours = Math.floor(totalMillis / 60 / 60 / 1000);
-
-      return {
-        seconds: totalSeconds % 60,
-        minutes: totalMinutes % 60,
-        hours: totalHours,
-      };
-    },
-    [currentTime, endTime]
-  );
-
-
   return (
     <ul id='reduct-preferences'>
-      <li>
-        <span className='reduct-preference-name'>time remaining:</span>
-        &nbsp;
-        <span>
-          {
-            remainingTime.hours > 1 ? `${remainingTime.hours} hours, ${remainingTime.minutes} minutes`
-              : remainingTime.hours > 0 ? `1 hour, ${remainingTime.minutes} minutes`
-                : remainingTime.minutes > 0 ? `${remainingTime.minutes} minutes`
-                  : `${remainingTime.seconds} seconds`
-          }
-        </span>
-      </li>
       <li>
         <span className='reduct-preference-name'>sound enabled:</span>
         &nbsp;
@@ -96,7 +43,7 @@ const PreferencesImpl: React.FC<PreferencesProps> = (props) => {
         &nbsp;
         <input
           type='checkbox'
-          checked={isResearchEnabled}
+          checked={isResearchEnabled === true}
           disabled
         />
       </li>
@@ -116,7 +63,6 @@ export const Preferences = connect(
   (store: DeepReadonly<GlobalState>) => ({
     isSoundEnabled: store.preferences.enableSounds,
     isResearchEnabled: store.preferences.enableResearch,
-    endTime: store.stats.startTime ? +store.stats.startTime + GAME_TIME_LIMIT : null,
   }),
   (dispatch) => ({
     enableSound(enabled: boolean) { dispatch(createEnableSound(enabled)); },
---
+++ client/src/view/stage/ui/tabs/menu/timer.tsx
@@ -0,0 +1,68 @@
+import React, {
+  useCallback, useState, useEffect, useRef, useMemo,
+} from 'react';
+import { connect } from 'react-redux';
+
+import { DeepReadonly } from '@/util/helper';
+import { GlobalState } from '@/store/state';
+import { GAME_TIME_LIMIT } from '@/util/constants';
+
+interface TimerStoreProps {
+  endTime: number;
+}
+
+const TimerImpl: React.FC<TimerStoreProps> = (props) => {
+  const { endTime } = props;
+
+  const [currentTime, setCurrentTime] = useState(+new Date());
+
+  const timerRef = useRef<number>();
+
+  const timerCallback = useCallback(() => {
+    setCurrentTime(+new Date());
+  }, []);
+
+  useEffect(() => {
+    if (currentTime < endTime) {
+      timerRef.current = setTimeout(timerCallback, 1000) as unknown as number;
+
+      return () => {
+        clearTimeout(timerRef.current);
+        timerRef.current = undefined;
+      };
+    }
+  }, [currentTime, endTime, timerCallback]);
+
+  const remainingTime = useMemo(
+    () => {
+      const totalMillis = endTime - currentTime;
+      const totalSeconds = Math.floor(totalMillis / 1000);
+      const totalMinutes = Math.floor(totalMillis / 60 / 1000);
+      const totalHours = Math.floor(totalMillis / 60 / 60 / 1000);
+
+      return {
+        seconds: totalSeconds % 60,
+        minutes: totalMinutes % 60,
+        hours: totalHours,
+      };
+    },
+    [currentTime, endTime]
+  );
+
+  return (
+    <span>
+      {
+        remainingTime.hours > 1 ? `${remainingTime.hours} hours, ${remainingTime.minutes} minutes`
+          : remainingTime.hours > 0 ? `1 hour, ${remainingTime.minutes} minutes`
+            : remainingTime.minutes > 0 ? `${remainingTime.minutes} minutes`
+              : `${remainingTime.seconds} seconds`
+      }
+    </span>
+  );
+};
+
+export const Timer = connect(
+  (store: DeepReadonly<GlobalState>) => ({
+    endTime: store.stats.startTime ? +store.stats.startTime + GAME_TIME_LIMIT : null,
+  })
+)(TimerImpl);
