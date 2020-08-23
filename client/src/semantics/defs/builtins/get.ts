93499bf86fe35b254235647762602bef50fde78b
Ibiyemi Abiodun <iaa34@cornell.edu>
2020-08-23 18:55:56 -0400

    Add dereferencing to get builtin

--- client/src/semantics/defs/builtins/get.ts
+++ client/src/semantics/defs/builtins/get.ts
@@ -1,3 +1,7 @@
+import produce from 'immer';
+
+import { ReferenceNode } from '../reference';
+
 import { addClonedNodes } from './util';
 
 import { BuiltInIdentifierNode } from '.';
@@ -6,6 +10,7 @@ import { BuiltInError, WrongBuiltInParamsCountError, WrongTypeError } from '@/st
 import { GameState } from '@/store/state';
 import { DeepReadonly, DRF } from '@/util/helper';
 import { cloneNodeAndAddDeep } from '@/util/nodes';
+import { createReferenceNode } from '@/semantics/util';
 
 export function builtinGet(self: DRF<BuiltInIdentifierNode>,
   args: DRF[],
@@ -13,11 +18,17 @@ export function builtinGet(self: DRF<BuiltInIdentifierNode>,
   if (args.length !== 2)
     throw new WrongBuiltInParamsCountError(self.id, 2, args.length);
 
-  const arrayNode = args[0];
+  let arrayNode = args[0];
   const indexNode = args[1];
 
-  if (arrayNode.type !== 'array')
-    throw new WrongTypeError(arrayNode.id, 'array', arrayNode.type);
+  // dereference param if we were given a reference to an array
+  while (arrayNode.type !== 'array') {
+    if (arrayNode.type !== 'reference') {
+      throw new WrongTypeError(arrayNode.id, 'array', arrayNode.type);
+    }
+
+    arrayNode = state.nodes.get(arrayNode.fields.target)!;
+  }
 
   if (indexNode.type !== 'number')
     throw new WrongTypeError(indexNode.id, 'number', indexNode.type);
@@ -28,6 +39,19 @@ export function builtinGet(self: DRF<BuiltInIdentifierNode>,
   if (index >= length)
     throw new BuiltInError(indexNode.id, `You tried to get item ${index} of an array with only ${length} items`);
 
-  const result = cloneNodeAndAddDeep(arrayNode.subexpressions[index], state.nodes);
-  return addClonedNodes(self, result, state);
+  const targetNode = state.nodes.get(arrayNode.subexpressions[index])!;
+
+  // if this points to a reference type, return a reference to it
+  if (targetNode.type === 'array') {
+    const referenceNode = createReferenceNode(targetNode.id) as DRF<ReferenceNode>;
+
+    return produce(state, draft => {
+      draft.nodes.set(referenceNode.id, referenceNode);
+      draft.added.set(referenceNode.id, self.id);
+      draft.returned = referenceNode.id;
+    });
+  } else {
+    const result = cloneNodeAndAddDeep(targetNode.id, state.nodes);
+    return addClonedNodes(self, result, state);
+  }
 }
